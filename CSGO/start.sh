df1347a9734897ed0add3b56ad8779b4ddedfb56
Jon Sands <fohdeesha@gmail.com>
2019-01-05 05:52:08 -0500

    re-add voting and fix start map

--- CSGO/start.sh
+++ CSGO/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sh /home/jon/csgo/go/srcds_run -game csgo -console -usercon +game_type 0 +game_mode 1 -tickrate 128 +ip 66.254.110.110 -maxplayers_override 32 +host_workshop_collection 556792439 +workshop_start_map 125995702
+sh /home/jon/csgo/go/srcds_run -game csgo -console -usercon +game_type 0 +game_mode 1 -tickrate 128 +ip 66.254.110.110 -maxplayers_override 32 +host_workshop_collection 556792439 +workshop_start_map 868124289
